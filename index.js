9339a91e1ae9881d9dc44cc9abcaf70486b816ae
praka07 <prakashbtw.007@gmail.com>
2018-04-06 10:49:07 +0200

    error

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express= require('express');
-var app=express();
-app.get('/',(req,res) =>{
-res.send('Hello World');
+var express = require('express');
+var app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
-app.listen(process.env.PORT||4040,() =>{
+app.listen(process.env.PORT || 4040, () => {
     console.log(`server is running`);
 });
